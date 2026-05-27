ef728fa9ffd78933c53fe565bea7fd50ea4de950
Kathyy-b <katrodriguez626@gmail.com>
2026-05-26 18:08:44 -0600

    first commit

---
+++ program.cs.m
@@ -0,0 +1,16 @@
+/*
+namespace => agrupar clases que tienen un dominio relacionado 
+class => define plantilla 
+main => punto de entrada de programa
+console.writeline =>
+*/
+namespace IntroduccionCSharp
+{
+  Class Program // ✅ pascal case ClientDetail - snake case client_detail
+{
+    static void Main(string[] args)
+    {
+        console.WriteLine("¡Hola Mundo!");
+    }
+}  
+}
